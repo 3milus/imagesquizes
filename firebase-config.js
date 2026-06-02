d5807bb18c6cc2256150c9e8b30f7989c6b26114
3milus <emil_haugstrup@hotmail.com>
2026-06-02 10:13:54 +0200

    added firebase values

--- firebase-config.js
+++ firebase-config.js
@@ -24,11 +24,11 @@
 // }
 // ─────────────────────────────────────────────────────────────────────────────
 const FIREBASE_CONFIG = {
-  apiKey:            'YOUR_API_KEY',
-  authDomain:        'YOUR_PROJECT_ID.firebaseapp.com',
-  databaseURL:       'https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com',
-  projectId:         'YOUR_PROJECT_ID',
-  storageBucket:     'YOUR_PROJECT_ID.appspot.com',
-  messagingSenderId: 'YOUR_SENDER_ID',
-  appId:             'YOUR_APP_ID'
+  apiKey:            'AIzaSyCSd_F0czxhYthUQ_wM1_nTERALgxscBt0',
+  authDomain:        'imagesquizzes.firebaseapp.com',
+  databaseURL:       'https://imagesquizzes-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId:         'imagesquizzes',
+  storageBucket:     'imagesquizzes.firebasestorage.app',
+  messagingSenderId: '606575268785',
+  appId:             '1:606575268785:web:21f7564e2bb347c295e390'
 };
